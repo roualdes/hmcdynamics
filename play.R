514d9a5a24413452a728e5ad034c00a1fff2b230
Edward A. Roualdes <eroualdes@csuchico.edu>
2020-05-14 15:46:49 -0700

    add simulation code for german_credit model

--- play.R
+++ play.R
@@ -3,15 +3,15 @@ set_cmdstan_path("~/cmdstan")
 theme_set(theme_minimal())
 replications <- 25
 throwaway <- 5
-
-## simulation
-branch <- "proposal"
 metric <- "dense_e"
 
+branch <- "proposal"
+
+## experiments
 ## dim <- 8; rho <- 0; nu <- 3; rep <- 10; model <- "german_credit"
 
+## fake
 for (model in c("gaussian", "student_t")) {
-
     force_recompile("~/hmcdynamics", model)
     mod <- cmdstan_model(glue("{model}.stan"))
 
@@ -21,8 +21,10 @@ for (model in c("gaussian", "student_t")) {
                 seed <- use_seed(model, branch, dim, rho, rep)
 
                 fit <- mod$sample(data = gendata(model, dim, rho), num_cores = 4,
-                                  num_warmup = 5000, num_samples = 5000, adapt_delta = 0.95,
-                                  seed = seed, metric = metric, refresh = 5000)
+                                  num_warmup = 10000, num_samples = 10000,
+                                  seed = seed, metric = metric,
+                                  refresh = 10000)
+
                 ## discard first throwaway runs
                 if (rep > throwaway) {
                     write_output(fit, model, branch, dim, rho, rep - throwaway)
@@ -31,6 +33,36 @@ for (model in c("gaussian", "student_t")) {
         }
     }
 }
+
+## german credit
+model <- "german_credit"
+df <- read_csv("german_credit.csv")
+N <- nrow(df)
+M <- ncol(df)
+x <- as.matrix(df[,-M])
+for (m in 1:(M - 1)) {
+    x[,m] <- 2 * (x[,m] / (diff(range(x[,m])))) - 1
+}
+
+set.seed(26606)
+seeds <- sample(1e5, replications)
+data <- list(y = df %>% pull(X25) - 1, x = cbind(1, x), n = N, d = M)
+
+force_recompile("~/hmcdynamics", model)
+mod <- cmdstan_model(glue("{model}.stan"))
+
+for (rep in 1:replications) {
+    fit <- mod$sample(data = data, num_cores = 4,
+                      num_warmup = 10000, num_samples = 10000,
+                      adapt_delta = 0.95, seed = seeds[rep], metric = metric,
+                      refresh = 10000)
+
+    ## discard first throwaway runs
+    if (rep > throwaway) {
+        write_output(fit, model, branch, dim, rho, rep - throwaway)
+    }
+}
+
 
 ## pre analysis
 rhos <- seq(0, 0.75, by=0.25)
